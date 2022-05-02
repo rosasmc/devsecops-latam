ae45913f6576b162bceb1934b25668bd9adabf31
rosasmc <macru35@yahoo.com>
2022-05-02 15:36:47 -0400

    primer commit landingPage DSC-Latam

---
+++ js/main.js
@@ -0,0 +1,104 @@
+;(function () {
+
+    'use strict';
+
+    var carousels = function() {
+        $(".owl-carousel1").owlCarousel(
+            {
+                loop:true,
+                center: true,
+                margin:0,
+                responsiveClass:true,
+                nav:false,
+                responsive:{
+                    0:{
+                        items:1,
+                        nav:false
+                    },
+                    600:{
+                        items:1,
+                        nav:false
+                    },
+                    1000:{
+                        items:3,
+                        nav:true,
+                        loop:false
+                    }
+                }
+            }
+        );
+
+        $(".owl-carousel2").owlCarousel(
+            {
+                loop:true,
+                center: false,
+                margin:0,
+                responsiveClass:true,
+                nav:true,
+                responsive:{
+                    0:{
+                        items:1,
+                        nav:false
+                    },
+                    600:{
+                        items:2,
+                        nav:false
+                    },
+                    1000:{
+                        items:3,
+                        nav:true,
+                        loop:true
+                    }
+                }
+            }
+        );
+    }
+
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+	
+	};
+
+
+    // svg responsive in mobile mode
+    var checkPosition = function() {
+        if ($(window).width() < 767) {
+            $("#bg-services").attr("viewBox", "0 0 1050 800");
+
+        }
+    };
+
+    (function($) {
+        carousels();
+        checkPosition();
+    })(jQuery);
+
+
+}());
+
+// menu toggle button
+function myFunction(x) {
+    x.classList.toggle("change");
+}
